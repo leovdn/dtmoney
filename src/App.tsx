5e13443abcd344e0f0a7bb49a94c266011095772
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2022-02-18 11:27:40 -0300

    global css config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
   return (
     <div className="App">
       <h1>Create React App</h1>
+      <GlobalStyle />
     </div>
   );
 }
